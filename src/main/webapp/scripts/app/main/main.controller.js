b6a3a3285a1a75e30a6f135b4cbe1fe824ce7a83
Miguel Pérez <miguelperezalo@gmail.com>
2016-03-10 14:20:19 +0100

    Creada tabla perfiles, flipcards hechas

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('plumbeerApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, $http) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+        $http.get("/api/users").then(function(response) {
+            $scope.users = response.data;
+        });
     });
+
