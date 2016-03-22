ce8db8d3345dfba03121c8b12cbcb63c447f8f7b
Miguel Pérez <miguelperezalo@gmail.com>
2016-03-23 00:05:36 +0100

    Incluido Login en la página de inicio, eliminados los bubbles

---
+++ src/main/webapp/JS/mostrarLoginRegister.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Miguel on 22/03/2016.
+ */
+$(document).ready(iniciar);
+
+function iniciar(){
+
+    $("#login").click(mostrarLogin);
+}
+
+function mostrarLogin(){
+
+    $("#logform").css("display","inline-block");
+
+}
