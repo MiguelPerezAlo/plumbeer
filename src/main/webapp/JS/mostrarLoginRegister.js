932853ca80f5aa90165e9b6b8e92110ee4aaab02
Miguel Perez <miguelperezalo@gmail.com>
2016-03-29 18:20:11 +0200

    Animacion de color en boton y formulario de registro incluido

--- src/main/webapp/JS/mostrarLoginRegister.js
+++ src/main/webapp/JS/mostrarLoginRegister.js
@@ -5,7 +5,11 @@ $(document).ready(iniciar);
 
 function iniciar(){
 
-    $("#login").click(moveContainer);
+    $(".bton").click(moveContainer);
+    $("#login").mouseenter(invertirColores);
+    $("#login").mouseleave(colorNormal);
+    $("#register").mouseenter(invertirColores);
+    $("#register").mouseleave(colorNormal);
 }
 
 function moveContainer(){
@@ -14,7 +18,15 @@ function moveContainer(){
 }
 
 function mostrarLogin(){
-
     $(".forms").css("display","inline-block");
+}
 
+function invertirColores(){
+    $(this).css("backgroundColor","rgba(255,255,255)");
+    $(this).css("color","rgb(44,219,246)");
+}
+
+function colorNormal(){
+    $(this).css("color","rgb(255,255,255)");
+    $(this).css("backgroundColor","rgba(44,219,246,0.2)");
 }
